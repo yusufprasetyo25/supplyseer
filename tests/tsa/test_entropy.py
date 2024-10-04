dd6d5b40a0b6b8d79e6afaa7bae86af13d6af209
Rostami Jako (BT: AI, Data & Analytics: Appl Data Eng, Data Science & ML) <jako.rostami@hm.com>
2024-10-05 01:29:49 +0200

    added tests for metrics

---
+++ tests/tsa/test_entropy.py
@@ -0,0 +1,27 @@
+from ordpy import complexity_entropy
+from typing import Union, List, Optional
+import numpy as np
+import pandas as pd
+import polars as pl
+
+from src.supplyseer.tsa.advanced.entropy import shannon_entropy, permutation_complexity
+
+
+
+def test_entropy_metrics():
+
+    x = np.linspace(1, 1000, 1000)
+    xmax_expected, xmin_expected = 1000, 1
+
+    assert x.max() == xmax_expected
+    assert x.min() == xmin_expected
+
+
+    shannon_entropy_output = shannon_entropy(x)
+    permutation_entropy = permutation_complexity(x)[0]
+    statistical_complexity = permutation_complexity(x)[1]
+
+
+    assert shannon_entropy_output == np.float64(9.687850776045513)
+    assert permutation_entropy == np.float64(0.0)
+    assert statistical_complexity == np.float64(0.0)
