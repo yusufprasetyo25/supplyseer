8c3910f5227da5a47d0dd08e78c49240b48df981
Rostami Jako (BT: AI, Data & Analytics: Appl Data Eng, Data Science & ML) <jako.rostami@hm.com>
2024-10-05 20:45:20 +0200

    added Takens embedding with test

--- src/supplyseer/wrangling/functionals.py
+++ src/supplyseer/wrangling/functionals.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import polars as pl
+import numpy as np
 
 def fill_missing_dates(input_data, time_column, period_fill, group_by_columns, keep_order):
     """
@@ -60,4 +61,31 @@ def fill_missing_dates(input_data, time_column, period_fill, group_by_columns, k
     return input_data
 
 
-    
+# TODO: Add more functionality with higher delays and higher dimensions
+# Do not change the list comprehension as it is the most efficient way to do this without adding complexity
+# Takes about 5 seconds to run on 1 million data points on a M1 Macbook Pro
+def univariate_takens(input_data):
+    """
+    Takens embedding for univariate time series data, currently only takes delay=1 and dimension=3 for 
+    1D data. Future iterations will include more flexibility.
+
+    Based on the simple calculation of 
+    x(t) - x(t-1), 
+    x(t-1) - x(t-2), 
+    x(t-2) - x(t-3) for a 1D time series data.
+
+    Args:
+        input_data (np.array): 1D time series data
+
+    Returns:
+        np.array: 2D array of Takens embedding data
+    """
+
+    if input_data.shape[1] != 1:
+        raise ValueError("Input data must be 1D. Please reshape data to 1D. Hint: input_data.reshape(-1, 1).")
+
+    x = [input_data[i] - input_data[i-1] for i in range(1, len(input_data)-2)]
+    y = [input_data[i] - input_data[i-1] for i in range(1+1, len(input_data)-1)]
+    z = [input_data[i] - input_data[i-1] for i in range(1+2, len(input_data))]
+
+    return np.concatenate([x, y, z], axis=1)
--- tests/wrangling/test_functionals.py
+++ tests/wrangling/test_functionals.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import polars as pl
+import numpy as np
 
-from src.supplyseer.wrangling.functionals import fill_missing_dates
+from src.supplyseer.wrangling.functionals import fill_missing_dates, univariate_takens
 
 
-def test_missing_dates():
+def test_functionals():
+
+    x = np.random.normal(0, 1, 10000).reshape(-1,1)
+    # x = np.array([np.nan]*10000).reshape(-1,1) # Uncomment this line to test for NaNs
+    takens_rows, takens_columns = x.shape[0] - 3, 3
 
     data = {
     "date": ["2021-01-01", "2021-01-03", "2021-01-05", "2021-01-07", "2021-01-09", "2021-01-11"],
@@ -21,5 +26,12 @@ def test_missing_dates():
     df_pl = pl.from_pandas(df)
     df_pl = fill_missing_dates(df_pl, time_column="date", period_fill="1d", group_by_columns=["country", "location"], keep_order=True)
 
+    takensarr = univariate_takens(x)
+
+
+
     assert isinstance(df, pd.DataFrame)
     assert isinstance(df_pl, pl.DataFrame)
+    assert takensarr.shape == (takens_rows, takens_columns), "Takens embedding array shape is not correct. Did you change 'univariate_takens()' function?"
+    assert np.isnan(takensarr).sum() == 0, "NaNs are present in the Takens embedding array. Please check the univarate_takens function"
+
